c0e370fa73190f8e9a74e3b09a55c3cd4d0645bd
ArjunSekar7 <arjun.sekar@aspiresys.com>
2019-06-14 10:28:41 +0530

    PHP function  program

---
+++ arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+	echo "Indexed Array";
+	$user = array("Arjun","Sam","Kumar");
+	echo "$user[0]";
+	echo "<br>";
+	echo "$user[1]";
+	echo "Assocative Array";
+	$array = array("Arjun" => 21);
+	echo "<br>";
+	echo array["Arjun"];
+	
+
+?>
